47e161ac7ee32e1a942bb352bfaacfa8e0fbda59
Carolina Muñoz <caromz2304@gmail.com>
2026-06-26 11:40:34 -0300

    Completa endpoints de carritos en memoria

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -12,49 +12,150 @@ router.post("/", (req, res) => {
     newId = 1;
   }
 
-  const listProducts = []
+  const listProducts = [];
 
   const newCart = {
     id: newId,
-    products: listProducts
-  }
+    products: listProducts,
+  };
 
-  carts.push(newCart)
+  carts.push(newCart);
 
-  res.status(201).json(newCart)
+  res.status(201).json(newCart);
 });
 
 // Mostrar carrito según id
 router.get("/:cid", (req, res) => {
-    const cid = Number(req.params.cid)
-    const searchedCart = carts.find((cart) => cart.id === cid)
+  const cid = Number(req.params.cid);
+  const searchedCart = carts.find((cart) => cart.id === cid);
 
-    if(!searchedCart){
-        return res.status(404).json({error: "Carrito no encontrado"})
-    }
+  if (!searchedCart) {
+    return res.status(404).json({ error: "Carrito no encontrado" });
+  }
 
-    res.json(searchedCart)
-})
+  res.json(searchedCart);
+});
+
+// Actualizar un carrito
+router.put("/:cid", (req, res) => {
+  const cid = Number(req.params.cid);
+  const searchedCart = carts.find((cart) => cart.id === cid);
+
+  if (!searchedCart) {
+    return res.status(404).json({ error: "Carrito no encontrado" });
+  }
+
+  const products = req.body.products;
+
+  if (!Array.isArray(products)) {
+    return res
+      .status(400)
+      .json({ error: "Debe enviar una lista de productos" });
+  }
+
+  const validProducts = products.every(item => item.product && item.quantity > 0)
+
+  if(!validProducts){
+    return res.status(400).json({error: "Ingreso invalido"})
+  }
+
+  searchedCart.products = products;
+
+  res.json(searchedCart);
+});
 
 // Agregar producto a un carrito
 router.post("/:cid/products/:pid", (req, res) => {
-    const cid = Number(req.params.cid)
-    const pid = Number(req.params.pid)
-    const searchedCart = carts.find((cart) => cart.id === cid)
+  const cid = Number(req.params.cid);
+  const pid = Number(req.params.pid);
+  const searchedCart = carts.find((cart) => cart.id === cid);
 
-    if(!searchedCart){
-        return res.status(404).json({error: "Carrito no encontrado"})
-    }
+  if (!searchedCart) {
+    return res.status(404).json({ error: "Carrito no encontrado" });
+  }
 
-    const searchedProduct = searchedCart.products.find((item) => item.product === pid)
+  const searchedProduct = searchedCart.products.find(
+    (item) => item.product === pid,
+  );
 
-    if(searchedProduct){
-        searchedProduct.quantity = searchedProduct.quantity + 1
-    } else {
-        searchedCart.products.push({ product: pid, quantity: 1 })
-    }
+  if (searchedProduct) {
+    searchedProduct.quantity = searchedProduct.quantity + 1;
+  } else {
+    searchedCart.products.push({ product: pid, quantity: 1 });
+  }
 
-    res.json(searchedCart)
+  res.json(searchedCart);
+});
+
+// Vaciar un carrito
+router.delete("/:cid", (req, res) => {
+  const cid = Number(req.params.cid)
+  const searchedCart = carts.find((cart) => cart.id === cid)
+
+  if(!searchedCart){
+    return res.status(404).json({error: "Carrito no encontrado"})
+  }
+
+   searchedCart.products = []
+
+   res.json(searchedCart)
 })
 
+
+// Eliminar un producto del carrito
+router.delete("/:cid/products/:pid", (req, res) => {
+  const cid = Number(req.params.cid);
+  const pid = Number(req.params.pid);
+  const searchedCart = carts.find((cart) => cart.id === cid);
+
+  if (!searchedCart) {
+    return res.status(404).json({ error: "Carrito no encontrado" });
+  }
+
+  const searchedIndexProduct = searchedCart.products.findIndex(
+    (item) => item.product === pid,
+  );
+
+  if (searchedIndexProduct === -1) {
+    return res
+      .status(404)
+      .json({ error: "Producto no encontrado en el carrito" });
+  }
+
+  searchedCart.products.splice(searchedIndexProduct, 1);
+
+  res.json(searchedCart);
+});
+
+// Actualizar la cantidad de un producto en un carrito
+router.put("/:cid/products/:pid", (req, res) => {
+  const cid = Number(req.params.cid);
+  const pid = Number(req.params.pid);
+  const searchedCart = carts.find((cart) => cart.id === cid);
+
+  if (!searchedCart) {
+    return res.status(404).json({ error: "Carrito no encontrado" });
+  }
+
+  const searchedProduct = searchedCart.products.find(
+    (item) => item.product === pid,
+  );
+
+  if (!searchedProduct) {
+    return res
+      .status(404)
+      .json({ error: "Producto no encontrado en el carrito" });
+  }
+
+  const quantity = Number(req.body.quantity);
+
+  if (!quantity || quantity <= 0) {
+    return res
+      .status(400)
+      .json({ error: "La cantidad no puede ser 0 ni menos" });
+  }
+
+  searchedProduct.quantity = quantity;
+  res.json(searchedCart);
+});
 module.exports = router;
